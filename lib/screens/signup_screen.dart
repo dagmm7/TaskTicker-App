aa5008bba54c1a1d56d1f9dc550947ec02ecf8bf
Nebbu333 <nebilasalh@gmail.com>
2025-12-09 22:14:45 +0300

    creating signup page input boxes

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
+class SignUpScreen extends StatelessWidget {
+  SignUpScreen({super.key});
+
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Sign Up"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: "Name",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) => value!.isEmpty ? "Enter your name" : null,
+              ),
+              const SizedBox(height: 15),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(),
+                ),
